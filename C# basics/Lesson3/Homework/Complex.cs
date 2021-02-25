f4a21c98270ca4bebbdc63c77c534b585069207a
AdeliaSabirova <adeliafsabirova@gmail.com>
2021-02-25 10:41:33 +0300

    Add lesson 3

---
+++ C# basics/Lesson3/Homework/Complex.cs
@@ -0,0 +1,81 @@
+﻿namespace Homework
+{
+    class Complex
+    {
+        private double Re;
+        private double Im;
+
+        public Complex()
+        {
+            this.Re = 0;
+            this.Im = 0;
+        }
+
+        public Complex(double x, bool RealOrImaginary)
+        {
+            if (RealOrImaginary)
+                this.Re = x;
+            else
+                this.Im = x;
+        }
+
+        public Complex(double Re, double Im)
+        {
+            this.Re = Re;
+            this.Im = Im;
+        }
+
+        public Complex getValue()
+        {
+            return this;
+        }
+
+        public void setValue(Complex z)
+        {
+            this.Re = z.Re;
+            this.Im = z.Im;
+        }
+
+        public void setValue(double Re, double Im)
+        {
+            this.Re = Re;
+            this.Im = Im;
+        }
+
+        public static Complex operator +(Complex z1, Complex z2)
+        {
+            Complex r = new Complex();
+            r.Re = z1.Re + z2.Re;
+            r.Im = z1.Im + z2.Im;
+            return r;
+        }
+
+        public static Complex operator -(Complex z1, Complex z2)
+        {
+            Complex r = new Complex();
+            r.Re = z1.Re - z2.Re;
+            r.Im = z1.Im - z2.Im;
+            return r;
+        }
+
+        public static Complex operator *(Complex z1, Complex z2)
+        {
+            Complex r = new Complex();
+            r.Re = z1.Re * z2.Re - z1.Im * z2.Im;
+            r.Im = z1.Re * z2.Im + z1.Im * z2.Re;
+            return r;
+        }
+
+        public override string ToString()
+        {
+            if (this.Im < 0)
+                return $"{this.Re} - {-this.Im}i";
+            else if (this.Re == 0 && this.Im != 0)
+                return $"{this.Im}";
+            else if (this.Im == 0)
+                return $"{this.Re}";
+            else
+                return $"{this.Re} + {this.Im}i";
+        }
+    }
+}
---
+++ C# basics/Lesson3/Practice/Table.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practice
+{
+    class Table
+    {
+        double a = -5;
+        double b = 5;
+        double h = 0.5;
+
+        public Table()
+        {
+        }
+        public Table(double a, double b, double h)
+        {
+            this.a = a;
+            this.b = b;
+            this.h = h;
+        }
+
+        double F(double x)
+        {
+            return 1 / x;
+        }
+
+        public void Show(double a, double b, double h)
+        {
+            Console.WriteLine("{0,10}{1,10}", "x", "F(x)");
+            for (double x = a; x <= b; x = x + h)
+            {
+                Console.WriteLine("{0,10}{1,10:f3}", x, F(x));
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("{0,10}{1,10}", "x", "F(x)");
+            for (double x = a; x <= b; x = x + h)
+            {
+                Console.WriteLine("{0,10}{1,10:f3}", x, F(x));
+            }
+        }
+    }
+}
