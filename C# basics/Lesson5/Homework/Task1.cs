1138c85647493d460b99712acf3c5b0f9814ac1b
AdeliaSabirova <adeliafsabirova@gmail.com>
2021-03-04 13:49:31 +0300

    Add lesson 5

---
+++ C# basics/Lesson5/Homework/Task1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Homework
+{
+    //Аделя Сабирова
+    static class Task1
+    {
+        /// <summary>
+        /// Создать программу, которая будет проверять корректность ввода логина. 
+        /// Корректным логином будет строка от 2 до 10 символов, содержащая только 
+        /// буквы латинского алфавита или цифры, при этом цифра не может быть первой.
+        /// с использованием регулярных выражений.
+        /// </summary>
+        /// <param name="filename"> File with list of logins</param>
+        public static void TaskReg(string filename)
+        {
+            Regex reg = new Regex(@"^[a-z][a-z0-9]{1,9}\b");
+            foreach(string line in File.ReadLines(filename))
+            {
+                if (reg.IsMatch(line))
+                    Console.WriteLine($"Login {line} is valid");
+                else Console.WriteLine($"Login {line} is not valid");
+            }
+        }
+
+        public static bool IsLatinLetter(char c)
+        {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+        }
+
+        /// <summary>
+        /// Создать программу, которая будет проверять корректность ввода логина. 
+        /// Корректным логином будет строка от 2 до 10 символов, содержащая только 
+        /// буквы латинского алфавита или цифры, при этом цифра не может быть первой.
+        /// без использования регулярных выражений.
+        /// </summary>
+        /// <param name="filename"> File with list of logins</param>
+        public static void Task(string filename)
+        {
+            foreach(string line in File.ReadLines(filename))
+            {
+                if (line.Length >= 2 && line.Length <= 10)
+                    if (!char.IsDigit(line[0]))
+                        if (line.All(c=> IsLatinLetter(c) || char.IsDigit(c)))
+                            Console.WriteLine($"Login {line} is valid");
+                        else Console.WriteLine($"Login {line} is not valid");
+                    else Console.WriteLine($"Login {line} is not valid");
+                else Console.WriteLine($"Login {line} is not valid");
+            }
+        }
+    }
+}
---
+++ C# basics/Lesson5/Homework/Task4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Homework
+{
+    partial class Program
+    {
+        static class Task4
+        {
+            struct Element
+            {
+                public string name;
+                public double averageScore;
+            }
+            /// <summary>
+            /// На вход программе подаются сведения о сдаче экзаменов учениками 9-х классов некоторой средней школы. 
+            /// В первой строке сообщается количество учеников N, которое не меньше 10, 
+            /// но не превосходит 100, каждая из следующих N строк имеет следующий формат:
+            /// Фамилия Имя оценки,
+            ///где Фамилия — строка, состоящая не более чем из 20 символов, 
+            ///Имя — строка, состоящая не более чем из 15 символов, 
+            ///оценки — через пробел три целых числа, соответствующие оценкам по пятибалльной системе. 
+            ///Фамилия и Имя, а также Имя и оценки разделены одним пробелом. 
+            ///Пример входной строки: Иванов Петр 4 5 3
+            ///Требуется написать как можно более эффективную программу, 
+            ///которая будет выводить на экран фамилии и имена трёх худших по среднему баллу учеников. 
+            ///Если среди остальных есть ученики, набравшие тот же средний балл, что и один из трёх худших, следует вывести и их фамилии и имена.
+            /// </summary>
+            /// <param name="filename">File with the data</param>
+            public static void Task(string filename)
+            {
+                int n = int.Parse(File.ReadLines(filename).First());
+                Element[] elements = new Element[n];
+                for (int i = 0; i < n; i++)
+                {
+                    string[] str = File.ReadLines(filename).ElementAtOrDefault(i + 1).Split(' ');
+                    elements[i].name = str[0] + ' ' + str[1];
+                    elements[i].averageScore = (double)(int.Parse(str[2])+int.Parse(str[3]) + int.Parse(str[4]))/3;
+                }
+                Array.Sort<Element>(elements, (x, y) => x.averageScore.CompareTo(y.averageScore));
+                int count = 1;
+                double min = elements[0].averageScore;
+                for (int i=0; i<elements.Length; i++)
+                {
+                    if (count == 3)
+                        break;
+                    else if (elements[i].averageScore == min)
+                            Console.WriteLine(elements[i].name);
+                        else if (elements[i].averageScore >min && count < 3)
+                        {
+                            min = elements[i].averageScore;
+                            Console.WriteLine(elements[i].name);
+                            count++;
+                        }
+                }
+            }
+        }
+    }
+}
