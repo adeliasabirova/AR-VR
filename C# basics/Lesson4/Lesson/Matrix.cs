5facc4ea33cdfbeb04bb8d75ca825ac28f46d3c0
AdeliaSabirova <adeliafsabirova@gmail.com>
2021-03-07 14:38:09 +0300

    Add lesson in lesson4

---
+++ C# basics/Lesson4/Lesson/Matrix.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lesson
+{
+    class Matrix
+    {
+        int[,] a; 
+        int[] Rows;   
+        public Matrix(int n, int m)
+        {
+            a = new int[n, m];
+            Random rnd = new Random();
+            Rows = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int s = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = rnd.Next(0, 10);
+                    s += a[i, j];
+                }
+                Rows[i] = s;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                    Console.Write("{0,4}", a[i, j]);
+                Console.WriteLine();
+            }
+        }
+
+        public int Search(out int count)
+        {
+            int min = int.MaxValue;
+            count = 0;         
+            foreach (int e in Rows)
+            {
+                if (e < min) min = e;
+            }
+            foreach (int e in Rows)
+            {
+                if (e == min) count++;
+            }
+            return min;
+        }
+        public void SearchRows()
+        {
+            int countRow;
+            int min = Search(out countRow); ;
+            for (int i = 0; i < Rows.Length; i++)
+            {
+                if (Rows[i] == min)
+                {
+                    Console.WriteLine("\n{0} {1}", i, Rows[i]);
+                }
+            }
+        }
+    }
+}
--- C# basics/Lesson4/Lesson/Program.cs
+++ C# basics/Lesson4/Lesson/Program.cs
@@ -1,7 +1,8 @@
-﻿ using System;
+﻿using System;
 
 namespace Lesson
 {
+
     class Program
     {
 
@@ -14,9 +15,50 @@ namespace Lesson
             Console.WriteLine(array.CountPositiv);
         }
 
+        static void Task2()
+        {
+            MyArray array = new MyArray("C:/Users/adely/source/repos/AR-VR/C# basics/Lesson4/Lesson/Task2.txt");
+            Console.WriteLine(array.ToString());
+            Console.WriteLine(array.Average);
+        }
+
+        static void Task3()
+        {
+            Random rand = new Random();
+            int[] a = new int[10];
+            for (int i = 0; i < 10; i++)
+                a[i] = rand.Next(0, 100);
+            foreach (var v in a)
+                Console.Write(v + " ");
+            int[] mass = new int[100];
+            foreach (var v in a) mass[v]++;
+            int imax = 0;
+            for (int i = 0; i < mass.Length; i++)
+                if (mass[i] > mass[imax]) imax = i;
+            for (int i = 0; i < mass.Length; i++)
+                if (mass[i] == mass[imax]) Console.WriteLine("\n" + i);
+        }
+
+        static void Task4()
+        {
+            MyArrayTwoDim a = new MyArrayTwoDim(2, 0, 10);
+            Console.WriteLine(a.ToString());
+            Console.WriteLine("\nМаксимальный элемент: " + a.Max);
+            Console.WriteLine("Минимальный элемент: " + a.Min);
+            Console.WriteLine("Среднее значение элементов: " + a.Average);
+        }
+
+        static void Task5()
+        {
+            Matrix a = new Matrix(5, 2);
+            a.Print();
+            a.SearchRows();
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
-            Task1();
+            Task5();
             Console.ReadKey();
         }
     }
